65e37fa5a1c8c07f12d74d386e40e2e6474d84b0
Filipos <83700574+pidizman@users.noreply.github.com>
2024-03-23 18:00:50 +0100

    modified scraper.py

--- apps/scraper/scraper.py
+++ apps/scraper/scraper.py
@@ -27,7 +27,31 @@ def scrape(url):
         "date": date,
         "time": time
     }
-    data.append(new)
+    return new
 
-    with open(json_filename, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+
+# def scrape(url):
+#     response = requests.get(url)
+#     html_content = response.content
+
+#     soup = BeautifulSoup(html_content, "html.parser")
+
+#     elements = soup.find_all("div", class_="row")
+#     #print(elements)
+
+#     temp = elements[0].find("div", class_="value").text
+#     dewpoint = elements[1].find("div", class_="value").text
+#     now = datetime.now()
+#     date = now.strftime("%d/%m/%Y")
+#     time = now.strftime("%H:%M:%S")
+
+#     new = {
+#         "temp": temp,
+#         "dewpoint": dewpoint,
+#         "date": date,
+#         "time": time
+#     }
+#     data.append(new)
+
+#     with open(json_filename, 'w') as json_file:
+#         json.dump(data, json_file, indent=4)
