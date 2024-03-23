73f4ff82de55fef735c1707785b51690e3ad87c6
Filipos <83700574+pidizman@users.noreply.github.com>
2024-03-23 18:00:31 +0100

    modified main.py

--- apps/scraper/main.py
+++ apps/scraper/main.py
@@ -1,5 +1,12 @@
 from scraper import scrape
+from flask import Flask
 
 url = "https://ws.meteoware.com/pn_v3_l.php?lg=en&unit=&fuid=352790&chk=7d879b&ut=c&up=hpa&ur=mm&layout=blue"
+app = Flask(__name__)
 
-scrape(url)
+@app.route("/")
+def home():
+    return scrape(url)
+
+if __name__ == '__main__':
+    app.run(port=5555)
