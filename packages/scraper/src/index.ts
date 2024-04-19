e9d52f8112931c613179cc924e372c21efbbb940
Filipos <83700574+pidizman@users.noreply.github.com>
2024-04-19 16:44:53 +0200

    make function to scrape data but dont write it into file

--- packages/scraper/src/index.ts
+++ packages/scraper/src/index.ts
@@ -11,7 +11,7 @@ interface Data {
   time?: string;
 }
 
-export async function scrapeData(URL: string) {
+export async function scrape(URL: string) {
   try {
     const response = await axios.get(URL);
 
@@ -40,6 +40,35 @@ export async function scrapeData(URL: string) {
     writeJson("./output.json", newFileData, (err) => {
       if (err) throw new Error(`${err}`);
     });
+  } catch (error) {
+    console.error("Chyba:", error);
+  }
+}
+
+export async function scrapeData(URL: string) {
+  try {
+    const response = await axios.get(URL);
+
+    const $ = cheerio.load(response.data);
+
+    const data: Data = {};
+
+    $(".row").each((index, element) => {
+      var label = $(element).find(".label").text().trim();
+      label = label.toLowerCase();
+
+      if (label === "dewpoint" || label === "temperature") {
+        const value = $(element).find(".value").text().trim();
+        data[label] = `${value}°C`;
+      }
+    });
+
+    const date = new Date();
+    data["date"] = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+    data["time"] =
+      `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+    return data;
   } catch (error) {
     console.error("Chyba:", error);
   }
