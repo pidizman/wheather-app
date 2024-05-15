e1af3e238041a0d8eefe5ecd95a20c66519c8f57
Filipos <83700574+pidizman@users.noreply.github.com>
2024-05-15 13:42:25 +0200

    starting making graph

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>Hello from Next.JS home page</h1>;
+  const ctx = document.getElementById("canvas");
+
+  return (
+    <>
+      <h1>Hello from Next.JS home page</h1>
+      <canvas id="canvas"></canvas>
+    </>
+  );
 }
