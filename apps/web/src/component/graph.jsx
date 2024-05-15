21f43e7db9b2f2d2acac42452d40c998f54b5bca
Filipos <83700574+pidizman@users.noreply.github.com>
2024-05-15 14:53:12 +0200

    .

--- apps/web/src/component/graph.jsx
+++ apps/web/src/component/graph.jsx
@@ -17,29 +17,10 @@ export default function Graph() {
 
       chartInstance.current = new Chart(ctx, {
         type: "line",
-        data: {
-          labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-          ],
-          datasets: [
-            {
-              label: "My First Dataset",
-              data: [65, 59, 80, 81, 56, 55, 40],
-              fill: false,
-              borderColor: "rgb(75, 192, 192)",
-            },
-          ],
-        },
+        data: {},
       });
     }
 
-    // Cleanup function to destroy chart instance when component unmounts
     return () => {
       if (chartInstance.current) {
         chartInstance.current.destroy();
